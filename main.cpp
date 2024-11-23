d2b53b73b82fdc55afdde834694c63039cfc5c0c
Артем <nickolawan2004@gmail.com>
2024-11-23 16:55:55 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,37 @@
-#include <stdio.h>
+#include <iostream>
 #include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
+using namespace std;
 
-typedef struct {
-    int data;
-} MyData;
+pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int ready = 0;
 
- 
-pthread_cond_t cond= PTHREAD_COND_INITIALIZER;
- 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
- int ready = 0;
- MyData *datas = NULL;
-
-
-void *provider(void *arg) {
-    for(int i=0; i<10;i++) {
-        sleep(1);  
-        pthread_mutex_lock(&mutex);
-        if (datas != NULL) free(datas);  
-        datas = (MyData*) malloc(sizeof(MyData));
-        datas->data = rand();  
-        printf("provided: %d\n", datas->data);
-        ready = 1;
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
+void* provider(void* arg) {
+  pthread_mutex_lock(&lock);
+  ready = 1; 
+  cout << "provided" << endl;
+  pthread_cond_signal(&cond1); 
+  pthread_mutex_unlock(&lock);
+  return nullptr;
 }
 
-void *consumer(void *arg) {
-    for(int i=0; i<10;i++) {
-        pthread_mutex_lock(&mutex);
- 
-        while (ready == 0) {
-            pthread_cond_wait(&cond, &mutex);  
-        }
-        printf("awoke: %d\n", datas->data);
-        ready = 0;
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
+void* consumer(void* arg) {
+  pthread_mutex_lock(&lock);
+  while (ready == 0) {
+    pthread_cond_wait(&cond1, &lock);
+    cout << "awoke" << endl;
+  }
+  ready = 0;
+  cout << "consumed" << endl;
+  pthread_mutex_unlock(&lock);
+  return nullptr;
 }
 
 int main() {
-    pthread_t provider_thread, consumer_thread;
-    pthread_cond_init(&cond, NULL);
-    pthread_mutex_init(&mutex, NULL);
-
-    pthread_create(&provider_thread, NULL, provider, NULL);
-    pthread_create(&consumer_thread, NULL, consumer, NULL);
-
-    pthread_join(provider_thread, NULL);
-    pthread_join(consumer_thread, NULL);
-
-    pthread_cond_destroy(&cond);
-    pthread_mutex_destroy(&mutex);
-    return 0;
+  pthread_t thread_provider, thread_consumer;
+  pthread_create(&thread_provider, nullptr, provider, nullptr);
+  pthread_create(&thread_consumer, nullptr, consumer, nullptr);
+  pthread_join(thread_provider, nullptr);
+  pthread_join(thread_consumer, nullptr);
+  return 0;
 }
