4bf86606e6d10da62e4d01dc5a1bdca82a0ed1e9
Артем <nickolawan2004@gmail.com>
2024-11-25 11:14:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,27 +4,30 @@ using namespace std;
 
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-int ready = 0;
+int ready1 = 0;  
+int ready2 = 0;  
 
 void* provider(void* arg) {
   pthread_mutex_lock(&lock);
-  while(ready == 1) { 
-    pthread_cond_wait(&cond1, &lock);
+  while(ready2 == 1) {  
+      pthread_cond_wait(&cond1, &lock);
   }
-  ready = 1; 
+  ready1 = 1;
+  ready2 = 0;  
   cout << "provided" << endl;
-  pthread_cond_signal(&cond1); 
+  pthread_cond_signal(&cond1);
   pthread_mutex_unlock(&lock);
   return 0;
 }
 
 void* consumer(void* arg) {
   pthread_mutex_lock(&lock);
-  while (ready == 0) {
+  while (ready1 == 0) {
     pthread_cond_wait(&cond1, &lock);
     cout << "awoke" << endl;
   }
-  ready = 0;
+  ready1 = 0;
+  ready2 = 1;  
   cout << "consumed" << endl;
   pthread_mutex_unlock(&lock);
   return 0;
@@ -36,5 +39,7 @@ int main() {
   pthread_create(&thread_consumer, nullptr, consumer, nullptr);
   pthread_join(thread_provider, nullptr);
   pthread_join(thread_consumer, nullptr);
+  pthread_cond_destroy(&cond1);
+  pthread_mutex_destroy(&lock);  
   return 0;
 }
